78f80c0d4042d36cfe6bf5558381b253160f9dba
Zach <thesonyman101@gmail.com>
2016-06-29 22:15:47 -0700

    Update arduinopassthough.ino

--- arduinopassthough.ino
+++ arduinopassthough.ino
@@ -1,6 +1,7 @@
 int input = 8;
 int p = 5;
 int n = 6;
+int dir
 void setup() {
 pinMode(p,OUTPUT);
 pinMode(n,OUTPUT);
@@ -9,7 +10,15 @@ pinMode(input,INPUT);
 }
 
 void loop() {
-digitalWrite(p,HIGH);
+dir = digitalRead(input);
+if (dir == HIGH) {
+  digitalWrite(p,HIGH);
+  digitalWrite(n,LOW);
+  print("forward");
+else {
 digitalWrite(p,LOW);
-
+digitalWrite(n,HIGH);
+print("backward");
+    }
+  }
 }
