4f6f8b5174b1b2f6a8b8d0678d1af687cff0f7d9
Zach <thesonyman101@gmail.com>
2016-06-29 21:41:31 -0700

    Create arduinopassthough.ino

---
+++ arduinopassthough.ino
@@ -0,0 +1,15 @@
+int input = 8;
+int p = 5;
+int n = 6;
+void setup() {
+pinMode(p,OUTPUT);
+pinMode(n,OUTPUT);
+pinMode(input,INPUT); 
+
+}
+
+void loop() {
+digitalWrite(p,HIGH);
+digitalWrite(p,LOW);
+
+}
